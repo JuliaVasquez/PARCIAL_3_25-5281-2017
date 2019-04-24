9a70ea12f5b9a0ad698bd1cf8c111fd372656aea
JuliaVasquez <47959536+JuliaVasquez@users.noreply.github.com>
2019-04-23 20:05:04 -0600

    Add files via upload

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            String usuario;
+            usuario = textBox1.Text.TrimEnd();
+            
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            String contraseña;
+            contraseña = textBox2.Text.TrimEnd();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("usuario: " + textBox1.Text+" "+"contraseña"+textBox2.Text);
+        }
+    }
+}
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            double Cantidad;
+            Cantidad = int.Parse(textBox3.Text);
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            double Porcentaje;
+            Porcentaje = int.Parse(textBox2.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
